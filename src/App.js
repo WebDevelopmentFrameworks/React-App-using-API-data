f219562f76a829822d7981d9d38f50ae3e2b9e6b
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-11-03 15:53:31 +0200

    deleted unnecessary files. Added env variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Link example
+const URL =
+  "https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
+      <p>{process.env.REACT_APP_API_KEY}</p>
     </div>
   );
 }
