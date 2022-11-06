ea46f6df3a5b2e58a94cfb58d08b56eecf7baa03
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-11-06 21:42:55 +0200

    Creted index.js to import all components and export them all at once

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header";
+import RecipeByName from "./RecipeByName";
+import Footer from "./Footer";
+import Home from "./Home";
+import About from "./About";
+
+export { Header, RecipeByName, Footer, Home, About };
